3c26486636e9983fd6c90091e9d514480947a182
liaoxingyu.lxy <924166518@qq.com>
2025-07-13 11:00:35 +0800

    add FluxKontextInpaintingConditioning

--- Model/common.py
+++ Model/common.py
@@ -1,9 +1,12 @@
 from .flux import *
+from .flux_kontext import *
 
 NODE_CLASS_MAPPINGS = {
     "ModelSamplingFluxNormalized": ModelSamplingFluxNormalized,
+    "KontextInpaintingConditioning": FluxKontextInpaintingConditioning,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "ModelSamplingFluxNormalized": "Smell Model Sampling Flux Normalized",
+    "KontextInpaintingConditioning": "Smell Kontext Inpainting Conditioning",
 }
