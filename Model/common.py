dc2f44ef56406b515bca0b03b4b29e9c89fcf97d
liaoxingyu.lxy <924166518@qq.com>
2025-07-19 17:09:56 +0800

    add kontext preset

--- Model/common.py
+++ Model/common.py
@@ -4,9 +4,11 @@ from .flux_kontext import *
 NODE_CLASS_MAPPINGS = {
     "ModelSamplingFluxNormalized": ModelSamplingFluxNormalized,
     "KontextInpaintingConditioning": FluxKontextInpaintingConditioning,
+    "KontextPresets": KontextPresets,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "ModelSamplingFluxNormalized": "Smell Model Sampling Flux Normalized",
     "KontextInpaintingConditioning": "Smell Kontext Inpainting Conditioning",
+    "KontextPresets": "Smell Kontext Presets",
 }
